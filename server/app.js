235080002343de9fb1bea13ef51af6f6dcd49c4b
Aaron Nguyen <aaronhtnguyen@gmail.com>
2025-05-31 00:16:37 -0700

    deleted unused files

--- server/app.js
+++ server/app.js
@@ -1,34 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const calendarRoutes = require('./routes/calendar');
-const syllabusRoutes = require('./routes/syllabus');
 const chatRoutes = require('./routes/chat');
 const authRoutes = require('./routes/auth');
 const cors = require('cors');
 
 const app = express();
 
-const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174'];
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-      return callback(new Error('Not allowed by CORS'));
-    },
+    origin: 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
   }),
 );
-
 
 app.use(bodyParser.json());
 
 app.use('/api/calendar', calendarRoutes);
-app.use('/api/syllabus', syllabusRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/chat', chatRoutes);
 
