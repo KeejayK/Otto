b9f16daf6c0b1672c1949611a097a6f2eec7d71f
Aaron Nguyen <aaronhtnguyen@gmail.com>
2025-05-31 00:16:37 -0700

    deleted unused files

--- server/app.js
+++ server/app.js
@@ -7,22 +7,13 @@ const cors = require('cors');
 
 const app = express();
 
-const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174'];
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-      return callback(new Error('Not allowed by CORS'));
-    },
+    origin: 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
   }),
 );
-
 
 app.use(bodyParser.json());
 
