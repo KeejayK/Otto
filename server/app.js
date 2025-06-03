bc88b8f318a98445f1083dfbca274de663ad4904
Keejay <KeejayK@gmail.com>
2025-06-02 20:50:44 -0700

    update allowedOrigins to include netlify

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 
 const app = express();
 
-const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174'];
+const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174', 'https://ottocalendar.netlify.app'];
 app.use(
   cors({
     origin: function (origin, callback) {
