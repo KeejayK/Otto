c7acf496b259ff408208e7a6629151aff09db699
Keejay <KeejayK@gmail.com>
2025-06-04 12:00:00 -0700

    remove unnecessary env check

--- server/firebase.js
+++ server/firebase.js
@@ -2,9 +2,6 @@ const admin = require('firebase-admin');
 
 
 const base64Json = process.env.FIREBASE_SERVICE_ACCOUNT_BASE64;
-if (!base64Json) {
-  throw new Error('FIREBASE_SERVICE_ACCOUNT_BASE64 is not defined');
-}
 
 const serviceAccount = JSON.parse(
   Buffer.from(base64Json, 'base64').toString('utf8')
